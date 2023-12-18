726847f6cb87fbcd8332dca8025d4cf91b13eeb1
Sumee Pokharel <sumeepokharel@Sumees-MacBook-Pro.local>
2023-12-18 14:19:25 -0600

    typescript type annotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,58 @@
 
-import {TNumber} from './type'
+import styles from './App.module.css';
+
 function App() {
-  //JS goes here
-  //Type inference
-//Type Annotation
-  let myCountry : TNumber
-  myCountry='USA'
-  myCountry='India'
-  myCountry='Nepal'
+  // type annotation: string
+  let myName: string;
+  myName = 'sumitra';
 
+  // type annotation: number
+  let myNumber: number;
+myNumber=20
 
-  let numbers:(number| string) []
-   numbers=[10,20,'30']
+  // accepting multiple types (using | union operator)
+  let age: string | number = 'Forty';
+  age = 40;
 
+  // to type Array for number only
+  let numbers: number[];
+  numbers = [10, 20, 30, 40, 50];
 
-   //objects
-   interface  IPerson {
-    age: number
-    name: String
-    favNumber?: number
-   }
+  // to type Array for number and String both
+  let myFavNumber: (number | string)[];
+  myFavNumber = [10, 'twenty', 'ten', 30];
 
-   let person: IPerson
-   person ={
-    age:20,
-    name: 'Alex',
-   }
-   person={
+  // typescript Boolean
+  let isPresent: boolean = true;
+
+  // typescript for object
+  interface IPerson {
+    age: number;
+    name: string;
+  }
+  let Person: IPerson;
+  Person = {
     age: 40,
-    name: 'Gorakh',
-    favNumber:7
-   }
+    name: 'Joanna',
+  };
+
   return (
     <>
-    <div>{myCountry}</div>
-    <div>{numbers}</div>
-  
+
+    <div className={styles.container}>
+        <h2> TypeScript</h2>
+        <div className={styles.myName}>{myName}</div>
+      <div className={styles.myNumber}>{myNumber}</div>
+      <div className={styles.age}>{age}</div>
+      <div className={styles.numbers}>{numbers.join(', ')}</div>
+      <div className={styles.myFavNumber}>{myFavNumber.join(', ')}</div>
+      <div className={styles.isPresent}>{isPresent.toString()}</div>
+      <div className={styles.person}>{Person.age}</div>
+      <div className={styles.person}>{Person.name}</div>
+
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
